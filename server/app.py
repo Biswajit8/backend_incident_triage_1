154100a8b7459a7c183955a1c6633195cfabf50d
Biswajit Saha <biswa.saha.707@gmail.com>
2026-04-10 23:13:11 +0530

    modifications for Not enough tasks with graders

--- server/app.py
+++ server/app.py
@@ -54,7 +54,7 @@ app = create_app(
 
 def main():
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
     main()
